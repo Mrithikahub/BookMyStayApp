a311d161c4fe2a09d6f878547d10dac84e42f506
Mrithikahub <mrithikakumar1112@gmail.com>
2026-03-19 00:26:58 +0530

    UC1: Application entry and welcome message

---
+++ src/BookMyStayApp.java
@@ -0,0 +1,14 @@
+/**
+ * Book My Stay App
+ * Version 1.0
+ * This is the entry point of the application
+ */
+public class BookMyStayApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Book My Stay App");
+        System.out.println("Version: 1.0");
+
+    }
+}
