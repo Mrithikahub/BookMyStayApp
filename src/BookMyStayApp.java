d106ffddb1533cade42df86356d31ed19bac34cd
Mrithikahub <mrithikakumar1112@gmail.com>
2026-03-19 00:28:35 +0530

    UC2: Room types with static availability

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,14 +1,66 @@
-/**
- * Book My Stay App
- * Version 1.0
- * This is the entry point of the application
- */
-public class BookMyStayApp {
+abstract class Room {
+    String type;
+    int price;
 
+    Room(String type, int price) {
+        this.type = type;
+        this.price = price;
+    }
+
+    abstract void display();
+}
+
+class SingleRoom extends Room {
+    SingleRoom() {
+        super("Single Room", 1000);
+    }
+
+    void display() {
+        System.out.println(type + " - Price: ₹" + price);
+    }
+}
+
+class DoubleRoom extends Room {
+    DoubleRoom() {
+        super("Double Room", 2000);
+    }
+
+    void display() {
+        System.out.println(type + " - Price: ₹" + price);
+    }
+}
+
+class SuiteRoom extends Room {
+    SuiteRoom() {
+        super("Suite Room", 5000);
+    }
+
+    void display() {
+        System.out.println(type + " - Price: ₹" + price);
+    }
+}
+
+public class BookMyStayApp {
     public static void main(String[] args) {
 
         System.out.println("Welcome to Book My Stay App");
-        System.out.println("Version: 1.0");
 
+        Room r1 = new SingleRoom();
+        Room r2 = new DoubleRoom();
+        Room r3 = new SuiteRoom();
+
+        // Static availability
+        int singleAvailable = 5;
+        int doubleAvailable = 3;
+        int suiteAvailable = 2;
+
+        r1.display();
+        System.out.println("Available: " + singleAvailable);
+
+        r2.display();
+        System.out.println("Available: " + doubleAvailable);
+
+        r3.display();
+        System.out.println("Available: " + suiteAvailable);
     }
 }
